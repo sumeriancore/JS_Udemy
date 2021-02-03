00ec14483e0c92b88195df060ebc8260afa6cb55
sumeriancore <stepan_stepan_1212@mail.ru>
2021-02-03 22:17:28 +0300

    12 lesson

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-"use strict";
+'use strict';
+
+let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", 0); 
+
+let personalMovieDB = {
+    count : numberOfFilms,
+    movies : {},
+    actors : {},
+    genres : [],
+    privat : false
+};
+
+for(let i = 0; i < 2; i++){
+    let movieName = prompt("Один из последних просмотренных фильмов?","");
+    let movieRate = prompt("На сколько его оцените?", "");
+
+    personalMovieDB.movies[movieName] = movieRate;
+}
+
+console.log(personalMovieDB.movies);
